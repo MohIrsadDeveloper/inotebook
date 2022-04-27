4c85f93bba3178722f48ff6d5d85db61001e03cd
Moh Irsad Idrisi <idrisimohdirshad@gmail.com>
2022-04-27 17:19:45 +0530

    create schema

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ const app = express();
 const connectToMongo = require('./db');
 connectToMongo();
 
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 app.get('/', (req,res) => {
     res.send("Hello world")
 })
